96a9eb6cf4f8127f31fbabb88dfe05d5e5fbff11
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2025-03-08 17:22:11 -0400

    packages updated.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
 import type { Config } from "tailwindcss";
+import { heroui } from '@heroui/react';
 
-export default {
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -14,5 +14,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui()],
 } satisfies Config;
