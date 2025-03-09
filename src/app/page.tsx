02f9769bf5076a6e44b43fa781131f9f3e5952c2
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2025-03-09 09:56:04 -0400

    GitHub authentication implemented.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
 import { Button } from '@heroui/button';
+import * as actions from '@/actions';
+import { auth } from '@/auth';
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await auth();
+
   return (
     <div>
+
       <div className="p-4 bg-blue-500 text-white text-center">
-        ¡Tailwind funciona!
+        ¡Tailwind works!
       </div>
-      <Button>Click me!</Button>
+
+      <form action={actions.signIn}>
+        <Button type="submit">Sign in</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Signed Out</div>
+      )}
     </div>
   );
 }
