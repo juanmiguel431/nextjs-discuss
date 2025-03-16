12d04972842bb484140bfe565b13809212ea012a
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2025-03-16 08:54:01 -0400

    Fetch Top Post implemented.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,16 @@ import TopicList from '@/components/topics/TopicList';
 import { Divider } from '@heroui/divider';
 import { Suspense } from 'react';
 import { Spinner } from '@heroui/spinner';
+import PostList from '@/components/posts/PostList';
+import { fetchTopPosts } from '@/db/queries/post';
 
 export default function Home() {
 
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
-        <h1 className="text-xl m-2">Topic Post</h1>
+        <h1 className="text-xl m-2">Top Post</h1>
+        <PostList fetchData={fetchTopPosts}/>
       </div>
       <div className="border shadow py-3 px-2">
         <TopicCreateFormPopover/>
--- src/db/queries/post.ts
+++ src/db/queries/post.ts
@@ -1,27 +1,39 @@
 import type { Post } from '@prisma/client';
 import { db } from '@/db';
 
-// export type PostWithData = Post & {
-//   topic: { slug: string; };
-//   user: { name: string | null; };
-//   _count: { comments: number; };
-// }
+export type PostWithData = Post & {
+  topic: { slug: string; };
+  user: { name: string | null; image: string | null };
+  _count: { comments: number; };
+}
 
-export type PostWithData = Awaited<
-  ReturnType<typeof fetchPostByTopicSlug>
->[number];
+// export type PostWithData = Awaited<
+//   ReturnType<typeof fetchPostByTopicSlug>
+// >[number];
 
-export function fetchPostByTopicSlug(slug: string) {
+export function fetchPostByTopicSlug(slug: string): Promise<PostWithData[]> {
   return db.post.findMany({
     where: { topic: { slug }},
     include: {
       topic: { select: { slug: true } },
-      user: { select: { name: true } },
+      user: { select: { name: true, image: true } },
       _count: { select: { comments: true } },
     }
   });
 }
 
 export async function fetchTopPosts(): Promise<PostWithData[]> {
-  return [];
+  return db.post.findMany({
+    orderBy: {
+      comments: {
+        _count: 'desc'
+      }
+    },
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true, image: true } },
+      _count: { select: { comments: true } },
+    },
+    take: 5
+  });
 }
